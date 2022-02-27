1a219747c56b3904a3eb2455f5872d1d3cfd32f5
bhi768 <bhimanand7@gmail.com>
2022-02-27 01:30:52 +0000

    vayu: use updated clang

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@
 ## Copy this script inside the kernel directory
 KERNEL_DEFCONFIG=vayu_user_defconfig
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
-FINAL_KERNEL_ZIP=Candy-Vayu-$(date '+%Y%m%d').zip
-export PATH="$HOME/Clang-15.0.0-20220212/bin:$PATH"
+FINAL_KERNEL_ZIP=SkyLine-Vayu-$(date '+%Y%m%d').zip
+export PATH="$HOME/Clang-15.0.0-20220226/bin:$PATH"
 export ARCH=arm64
 export SUBARCH=arm64
-export KBUILD_COMPILER_STRING="$($HOME/Clang-15.0.0-20220212/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+export KBUILD_COMPILER_STRING="$($HOME/Clang-15.0.0-20220226/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 
-#if ! [ -d "$HOME/Clang-15.0.0-20220212" ]; then
+#if ! [ -d "$HOME/Clang-15.0.0-20220226" ]; then
 #echo "Proton clang not found! Cloning..."
 #if ! git clone -q https://github.com/kdrag0n/proton-clang --depth=1 --single-branch ~/proton; then
 #echo "Cloning failed! Aborting..."
