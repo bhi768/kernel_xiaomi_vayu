698b55726d5eed0cc78097d972fae804a88546c6
bhi768 <bhimanand7@gmail.com>
2022-03-26 12:50:51 +0000

    update script

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@
 KERNEL_DEFCONFIG=vayu_user_defconfig
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
 FINAL_KERNEL_ZIP=SkyLine-Vayu-$(date '+%Y%m%d').zip
-export PATH="$HOME/Clang-15.0.0-20220318/bin:$PATH"
+export PATH="$HOME/clang-lyn/clang-lyn/bin:$PATH"
 export ARCH=arm64
 export SUBARCH=arm64
-export KBUILD_COMPILER_STRING="$($HOME/Clang-15.0.0-20220318/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+export KBUILD_COMPILER_STRING="$($HOME/clang-lyn/clang-lyn/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 
 #if ! [ -d "$HOME/Clang-15.0.0-20220212" ]; then
 #echo "Proton clang not found! Cloning..."
@@ -45,6 +45,7 @@ make -j$(nproc --all) O=out \
                       CROSS_COMPILE=aarch64-linux-gnu- \
                       CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
                       NM=llvm-nm \
+                      LD=ld.lld \
                       OBJCOPY=llvm-objcopy \
                       OBJDUMP=llvm-objdump \
                       STRIP=llvm-strip
